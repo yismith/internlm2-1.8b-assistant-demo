2912afccbcf930685eea25b5ac9647fd0903a89d
yismith <130582579+yismith@users.noreply.github.com>
2024-06-16 16:13:34 +0800

    1、更新gradio与transformers的版本 2、调整模型加载路径

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2-1.8b-assistant-demo to the base_path directory using git tool
 base_path = './internlm2-1.8b-assistant-demo'
+model_base_path = './internlm2-assistant-demo'
 os.system(f'git clone https://code.openxlab.org.cn/yismith/internlm2-1.8b-assistant-demo.git {base_path}')
-os.system(f'cd {base_path} && git lfs pull')
+os.system(f'cd {base_path} && git lfs pull && cd ..')
+os.system(f'mv {base_path} {model_base_path}')
 
-tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+tokenizer = AutoTokenizer.from_pretrained(model_base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
