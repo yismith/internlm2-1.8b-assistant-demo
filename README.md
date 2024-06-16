fde98c354ea77864bc7841b9227a63e73de61128
yismith <130582579+yismith@users.noreply.github.com>
2024-06-16 14:54:40 +0800

    1、增加internlm2-1.8b-assistant-demo相关代码与配置

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # internlm2-1.8b-assistant-demo
+
+## 记录
+推荐的目录结构
+```shell
+├─GitHub_Repo_Name
+│  ├─app.py                 # Gradio 应用默认启动文件为app.py，应用代码相关的文件包含模型推理，应用的前端配置代码
+│  ├─requirements.txt       # 安装运行所需要的 Python 库依赖（pip 安装）
+│  ├─packages.txt           # 安装运行所需要的 Debian 依赖项（ apt-get 安装）
+|  ├─README.md              # 编写应用相关的介绍性的文档
+│  └─... 
+```
+
+其他环境安装：若需要安装除了 Python 以外的包，如需要通过 mim 安装 mmcv，可先在 requirement.txt 中填写 mim，然后在 app.py 中写入以下代码，即可完成相关包的安装：
+```python
+import os
+os.system("mim install mmcv-full")
+```
---
+++ app.py
@@ -0,0 +1,23 @@
+import gradio as gr
+import os
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+
+# download internlm2-1.8b-assistant-demo to the base_path directory using git tool
+base_path = './internlm2-1.8b-assistant-demo'
+os.system(f'git clone https://code.openxlab.org.cn/yismith/internlm2-1.8b-assistant-demo.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
+
+tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+
+def chat(message,history):
+    for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
+        yield response
+
+gr.ChatInterface(chat,
+                 title="internlm2-1.8b-assistant-demo",
+                 description="""
+internlm2-1.8b微调后的个人小助理demo
+                 """,
+                 ).queue(1).launch()
